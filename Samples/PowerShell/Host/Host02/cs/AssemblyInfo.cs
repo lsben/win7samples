dabb8216b3e119dd98ca7bb871b7b52698df3364
Matthew Weaver <matthew@xnor.ai>
2017-09-25 15:20:33 -0700

    Trim the repo

--- Samples/PowerShell/Host/Host02/cs/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation
-
-using System.Reflection;
-using System.Security.Permissions;
-
-
-[assembly: AssemblyTitle("GetProcessSample01")]
-[assembly: AssemblyDescription("Get Process")]
--- Samples/PowerShell/Process/GetProcess02/cs/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2006 Microsoft Corporation
-
-using System.Reflection;
-using System.Security.Permissions;
-
-[assembly: AssemblyTitle("GetProcessSample02")]
-[assembly: AssemblyDescription("Get Process Sample")]
