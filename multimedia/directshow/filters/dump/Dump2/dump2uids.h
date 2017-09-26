b7bfd3b5dcbf66f18b471a4a2e11f05b9c3df0b9
Matthew Weaver <matthew@xnor.ai>
2017-09-26 10:42:41 -0700

    Cloned 'Dump' project to upgrade it to run in modern world.

---
+++ multimedia/directshow/filters/dump/Dump2/dump2uids.h
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+// File: Dump2UIDs.h
+//
+// Desc: DirectShow sample code - CLSIDs used by the dump renderer.
+//
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------------------------
+
+
+// { 36a5f770-fe4c-11ce-a8ed-00aa002feabf }
+DEFINE_GUID(CLSID_Dump2,
+0x36a5f770, 0xfe4c, 0x11ce, 0xa8, 0xed, 0x00, 0xaa, 0x00, 0x2f, 0xea, 0xbf);
+
